1e62be6d788c6a4f4d724e34976fd412efd4d014
redfushion <mario.alb.22@gmail.com>
2017-04-05 11:22:42 -0600

    05/04/17
    Modificacion

--- js/picture_upload.js
+++ js/picture_upload.js
@@ -1,18 +1,18 @@
 function enviar_imagen() 
 	{
-	var file = document.getElementById("photo").files[0]; //fetch file
-            var formData = new FormData();                     
-            formData.append('foto_tomada', file); //append file to formData object
+		var file = document.getElementById("photo").files[0]; //fetch file
+		var formData = new FormData();                     
+		formData.append('foto_tomada', file); //append file to formData object
 
-            $.ajax({
-                url: "http://185.139.239.250/html5_camera_upload/modelo/take_data.php",
-                type: "POST",
-                data: formData,
-                processData: false, //prevent jQuery from converting your FormData into a string
-                contentType: false, //jQuery does not add a Content-Type header for you
-                success: function (msg) {
-                    //alert(msg)
-					$("#responde").html(msg);
-                }
-            });
+		$.ajax({
+			url: "modelo/take_data.php",
+			type: "POST",
+			data: formData,
+			processData: false, //prevent jQuery from converting your FormData into a string
+			contentType: false, //jQuery does not add a Content-Type header for you
+			success: function (msg) {
+				//alert(msg);
+				$("#responde").html(msg);
+			}
+		});
 	}
