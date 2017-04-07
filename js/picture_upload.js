73568642452fc521d29c3f3b77d7239bde87ab18
redfushion <mario.alb.22@gmail.com>
2017-04-07 12:53:43 -0600

    07/04/17
    Modificacion

--- js/picture_upload.js
+++ js/picture_upload.js
@@ -8,28 +8,40 @@ function enviar_imagen()
 			url: "modelo/take_data.php",
 			type: "POST",
 			data: formData,
+			dataType: "json",
 			processData: false, //prevent jQuery from converting your FormData into a string
 			contentType: false, //jQuery does not add a Content-Type header for you
 			success: function (msg) {
 				//alert(msg);
-				$("#responde").html(msg);
-				
-				var networkState = navigator.connection.type;
-
-				var states = {};
-				states[Connection.UNKNOWN]  = 'Unknown connection';
-				states[Connection.ETHERNET] = 'Ethernet connection';
-				states[Connection.WIFI]     = 'WiFi connection';
-				states[Connection.CELL_2G]  = 'Cell 2G connection';
-				states[Connection.CELL_3G]  = 'Cell 3G connection';
-				states[Connection.CELL_4G]  = 'Cell 4G connection';
-				states[Connection.CELL]     = 'Cell generic connection';
-				states[Connection.NONE]     = 'No network connection';
-
-				alert('Connection type: ' + states[networkState]);
+				$('.nombre').html("Upload: "+data.nombre);
+				$('.tipo').html("Type: "+data.tipo);
+				$('.tamano').html("Size: "+data.tamano);
+				$("#responde").html("<img src='"+data.imagen+"'>");
 			}
 		});
 	}
+function mostrar_red(){
+	
+	function onDeviceReady() {
+		checkConnection();
+	}
+
+	function checkConnection() {
+		var networkState = navigator.connection.type;
+
+		var states = {};
+		states[Connection.UNKNOWN]  = 'Unknown connection';
+		states[Connection.ETHERNET] = 'Ethernet connection';
+		states[Connection.WIFI]     = 'WiFi connection';
+		states[Connection.CELL_2G]  = 'Cell 2G connection';
+		states[Connection.CELL_3G]  = 'Cell 3G connection';
+		states[Connection.CELL_4G]  = 'Cell 4G connection';
+		states[Connection.CELL]     = 'Cell generic connection';
+		states[Connection.NONE]     = 'No network connection';
+
+		alert('Connection type: ' + states[networkState]);
+	}
+}
 	document.addEventListener("deviceready", onDeviceReady, false);
 
     // device APIs are available
--- modelo/take_data.php
+++ modelo/take_data.php
@@ -1,14 +1,28 @@
 <?php
 //print_r($_FILES);
 
-	echo "Upload: " . $_FILES["foto_tomada"]["name"] . "<br>";
+	/*echo "Upload: " . $_FILES["foto_tomada"]["name"] . "<br>";
 	echo "Type: " . $_FILES["foto_tomada"]["type"] . "<br>";
 	echo "Size: " . ($_FILES["foto_tomada"]["size"] / 1024) . " kB<br>";
-	echo "Stored in: " . $_FILES["foto_tomada"]["tmp_name"] . "<br>";
-
+	echo "Stored in: " . $_FILES["foto_tomada"]["tmp_name"] . "<br>";*/
+	
+	header("content-type:application/json");
+	
+	$nombre = $_FILES["foto_tomada"]["name"];
+	$tipo = $_FILES["foto_tomada"]["type"];
+	$tamano = ($_FILES["foto_tomada"]["size"] / 1024)."kB";
 	$aExtraInfo = getimagesize($_FILES['foto_tomada']['tmp_name']);
 	$sImage = "data:" . $aExtraInfo["mime"] . ";base64," . base64_encode(file_get_contents($_FILES['foto_tomada']['tmp_name']));
-	echo '<p>The image has been uploaded successfully</p><p>Preview:</p><img src="' . $sImage . '" alt="Your Image" />';
+	
+	$cadena_imagen = array(
+		"nombre" => $nombre,
+		"tipo" => $tipo,
+		"tamano" => $tamano,
+		"imagen" => $sImage
+	);
+
+	
+	echo $cadena_imagen
 
 /*
 Tomar el archivo recivido como un string, ideal para fomatos de texto sin formato como txt
